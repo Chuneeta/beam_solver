cf7cb87beec0f7a4df62d1419d988138344b00d5
Chuneeta <cnunhokee@gmail.com>
2018-11-13 12:51:13 -0800

    Extracts fluc density of sources

--- beam_solver/extract.py
+++ beam_solver/extract.py
@@ -8,6 +8,7 @@ import scipy.stats as stats
 import numpy as np
 import pylab
 import copy
+import warnings
 
 def get_fitsinfo(fitsname):
     """
@@ -29,19 +30,21 @@ def get_fitsinfo(fitsname):
 
     return {'data':data, 'header':header, 'freq':freq, 'nxaxis':nxaxis, 'nyaxis':nyaxis}
     
-def get_center_radec(fitsfile):
+def get_centre_radec(fitsfile):
     """
     Return center ra and dec of any fitsfile
 
+    Parameters
+    ----------	
     fitsfile : string
         Name of fitsfile 
     """
     
     fitsinfo = get_fitsinfo(fitsfile)
-    w = wcs.WCS(fitsinfo['hdr'])
+    w = wcs.WCS(fitsinfo['header'])
     nxaxis = fitsinfo['nxaxis']
     nyaxis = fitsinfo['nyaxis']
-    cxpix, cypix = int(naxis/2.), int(nyaxis/2.)
+    cxpix, cypix = int(nxaxis/2.), int(nyaxis/2.)
     cra, cdec = w.all_pix2world(cxpix, cypix, 0, 0, 0)[:2]    
 
     return cra, cdec
@@ -89,61 +92,64 @@ def get_flux(fitsname, ra, dec, plot=False):
         ra_pix = int(ra_pix)
         dec_pix = int(dec_pix) 
 
-    # checking if the pixel are within the range
-    assert 0 <= ra_pix < nxaxis , 'Pixel value along the x-axis is outside the image range'
-    assert 0 <= dec_pix < nyaxis , 'Pixel value along the y-axis is outside the image range'
-
-    # selecting region with synthesized beam
-    l_axis = np.arange(0, nxaxis)
-    m_axis = np.arange(0, nyaxis)
-    ll, mm = np.meshgrid(l_axis, m_axis)
+    if (0 <= ra_pix < nxaxis) and ( 0 <= dec_pix < nyaxis):
+        # selecting region with synthesized beam
+        l_axis = np.arange(0, nxaxis)
+        m_axis = np.arange(0, nyaxis)
+        ll, mm = np.meshgrid(l_axis, m_axis)
         
-    R = np.sqrt((ll - ra_pix)**2 + (mm - dec_pix)**2)
-    select = R < bm_radius_px
+        R = np.sqrt((ll - ra_pix)**2 + (mm - dec_pix)**2)
+	select = R < 2 * bm_radius_px
     
-    imdata_select = imdata[select]
-    peakval = np.nanmax(imdata_select)
-    minval = np.nanmin(imdata_select)
-    std = np.nanstd(imdata_select)
-    rms = np.std(imdata[~select]) # calculated rm outside the selected region
-    peak_err = rms / np.sqrt(bm_npx / 2.0)
+        imdata_select = imdata[select]
+        peakval = np.nanmax(imdata_select)
+        minval = np.nanmin(imdata_select)
+        std = np.nanstd(imdata_select)
+        rms = np.sqrt(np.nanmean(imdata[~select])**2) # calculated rm outside the selected region
+        peak_err = rms / np.sqrt(bm_npx / 2.0)
 
-    peak_ind = np.argmax(imdata[select])
-    peak_xpix = ll[select][peak_ind]
-    peak_ypix = mm[select][peak_ind]
+        peak_ind = np.argmax(imdata[select])
+        peak_xpix = ll[select][peak_ind]
+        peak_ypix = mm[select][peak_ind]
 
-    # fitting 2D gaussian
-    gauss_init = mod.functional_models.Gaussian2D(peakval, ra_pix, dec_pix, x_stddev=bmaj_px/2, y_stddev = bmin_px/2)
-    fitter = mod.fitting.LevMarLSQFitter()
-    gauss_fit = fitter(gauss_init, ll[select], mm[select], imdata_select)
+        # fitting 2D gaussian
+        gauss_init = mod.functional_models.Gaussian2D(peakval, peak_xpix, peak_ypix, x_stddev=bmaj_px/2., y_stddev = bmin_px/2.)
+        fitter = mod.fitting.LevMarLSQFitter()
+        gauss_fit = fitter(gauss_init, ll[select], mm[select], imdata_select)
    
-    gauss_peak = gauss_fit.amplitude.value
-    gauss_xstd = gauss_fit.x_stddev.value
-    gauss_ystd = gauss_fit.y_stddev.value
-    beam_theta = hdr["BPA"] * np.pi / 180
-    P = np.array([ll - peak_xpix, mm - peak_ypix]).T
-    Prot = P.dot(np.array([[np.cos(beam_theta), -np.sin(beam_theta)], [np.sin(beam_theta), np.cos(beam_theta)]]))
-    gauss_cov = np.array([[gauss_xstd**2, 0], [0, gauss_ystd**2]])
-    model_gauss = stats.multivariate_normal.pdf(Prot, mean=np.array([0,0]), cov=gauss_cov)
-    model_gauss *= gauss_fit.amplitude.value / model_gauss.max()
-    gauss_int = np.nansum(model_gauss) / bm_npx
+        gauss_peak = gauss_fit.amplitude.value
+        gauss_xstd = gauss_fit.x_stddev.value
+        gauss_ystd = gauss_fit.y_stddev.value
+        beam_theta = hdr["BPA"] * np.pi / 180
+        P = np.array([ll - peak_xpix, mm - peak_ypix]).T
+        Prot = P.dot(np.array([[np.cos(beam_theta), -np.sin(beam_theta)], [np.sin(beam_theta), np.cos(beam_theta)]]))
+        gauss_cov = np.array([[gauss_xstd**2, 0], [0, gauss_ystd**2]])
 
-    if plot:
-        imdata_copy = copy.deepcopy(imdata)
-        imdata_copy[~select] = np.nan
+        try:
+            model_gauss = stats.multivariate_normal.pdf(Prot, mean=np.array([0,0]), cov=gauss_cov)
+            model_gauss *= gauss_fit.amplitude.value / model_gauss.max()
+            gauss_int = np.nansum(model_gauss) / bm_npx
+        
+            if plot:
+                imdata_copy = copy.deepcopy(imdata)
+                imdata_copy[~select] = np.nan
+            
+                fig = pylab.figure(figsize=(6, 4))
+                my_wcs = wcs.WCS(hdr, naxis=[wcs.WCSSUB_CELESTIAL])
+                ax = pylab.subplot(121, projection=my_wcs)
+                pylab.imshow(imdata_copy, aspect='auto', cmap='jet', origin='lower')
+                pylab.grid(color='black')
+                pylab.colorbar()
 
-        fig = pylab.figure(figsize=(6, 4))
-        my_wcs = wcs.WCS(hdr, naxis=[wcs.WCSSUB_CELESTIAL])
-        ax = pylab.subplot(121, projection=my_wcs)
-        pylab.imshow(imdata_copy, aspect='auto', cmap='jet', origin='lower')
-        pylab.grid(color='black')
-        pylab.colorbar()
-
-        ax = pylab.subplot(122, projection=my_wcs)
-        pylab.imshow(model_gauss.T, aspect='auto', cmap='jet', origin='lower')
-        pylab.grid(color='black')
-        pylab.colorbar()
-        pylab.show()
-
-    return {'freq':freq, 'peak':peakval, 'min':minval, 'peak_err':peak_err, 'gauss_peak':gauss_peak, 'gauss_int':gauss_int}
+                ax = pylab.subplot(122, projection=my_wcs)
+                pylab.imshow(model_gauss.T, aspect='auto', cmap='jet', origin='lower')
+                pylab.grid(color='black')
+                pylab.colorbar()
+                pylab.show()
+        except:
+            gauss_int = np.nan        
+    else:
+            warnings.warn('WARNING: Right ascension or declination outside image field, therefore values are set to nan', Warning)
+            peakval, minval, rms, gauss_peak, gauss_int = np.nan, np.nan, np.nan, np.nan, np.nan                
+    return {'freq':freq, 'peak':peakval, 'min':minval, 'rms':rms, 'gauss_peak':gauss_peak, 'gauss_int':gauss_int}
 
