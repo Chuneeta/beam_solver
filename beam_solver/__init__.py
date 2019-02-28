ddf6c374f2296122e9f04a3138b53b01e19ff674
Chuneeta <cnunhokee@gmail.com>
2019-02-28 13:50:26 -0800

    updated

--- beam_solver/__init__.py
+++ beam_solver/__init__.py
@@ -1,5 +1,5 @@
 """
 __init__.py file for beam_solver
 """
-from beam_solver import catdata, beam_utils, beamsolve, imaging, generate_resvis, uv_convert
+from beam_solver import catdata, fits_utils, beam_utils, extract, beamsolve, imaging, generate_resvis, uv_convert
 
