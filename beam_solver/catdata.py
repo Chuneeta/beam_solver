dcb463e17a543bf7dba9cd9da394f7b3382d356e
Chuneeta <cnunhokee@gmail.com>
2019-03-01 15:24:20 -0800

    combined repeated functionalities

--- beam_solver/catdata.py
+++ beam_solver/catdata.py
@@ -6,8 +6,7 @@ import os, sys
 import numpy as np
 import h5py
 import healpy
-
-pol2ind = {'xx':0, 'yy': 1}
+import copy
 
 class catData(object):
     """
@@ -82,83 +81,165 @@ class catData(object):
             jds.append(hdr['JD'])
         return jds
 
-    def gen_catalog(self, fitsfiles, ras, decs, flux_type='peak', return_data=False):
+    def _get_lstha(self, jd, ra):
         """
-        Extracts flux measurements at specified right ascension and declination values from the fitsfiles 
-        and generates a catdata object containing the data and necessary metadata for   xx or yy 
-        polarization. It can also return a dictionary containing the data and selected metadata.
+        Returns local sidereal time and hour angle in radians
+        jd : float
+            Julian date representing the observating time
+        ra : float
+            Right ascension in radians
+        """
+        lst = ct.jd2lst(jd)
+        ha = ct.ralst2ha(ra * np.pi/180, lst)
+        return lst, ha
+
+    def _get_azalt(self, dec, ha):
+        """
+        Return az alt coordinates in radians
+        Parameters
+        ---------
+        dec : float
+            Declination in radians.
+        ha : float 
+            Hour angle in radians.       
+        """
+        az, alt = ct.hadec2azalt(dec * np.pi/180., ha)
+        return az, alt
+
+    def _generate_srcdict(self, ras, decs, fitsfiles):
+        """
+        Extracts flux measurements at specified right ascension and declination values from the fitsfiles
+        and return dictionary containing the data and necessary metadata for single polarization.
+        polarization. 
         Parameters
         ---------
         fitsfiles : list of str
-            List of of xx or yy fitsfiles that will be used to generate or extract the source catalog.
+            List of of xx or yy fitsfiles that will be used to extract the flux values.
         ras : list of float
             List of right ascension values in degrees.
         decs : list of floats
             List of declination values in degrees.
-        flux_type : str
-            Type of flux density to store, can be either 'peak' or 'total'. 'peak' returns the peak
-            pixel value selected from all the pixels confined within the synthesized beam. 'total' 
-            returns the integrated flux density from a gaussian fit around the source.
-        return_data : boolean
-            If True, returns dictionary with the data values and selected metadata.
         """
-        assert len(ras) == len(decs), "Right ascenscion array should be of the same size as declination array."
         # selecting unique ras and decs
         nsrcs = len(ras)
-        nfits = len(fitsfiles)      
-        # initializating source dict and numpy arrays
+        nfits = len(fitsfiles)
         srcdict = OrderedDict()
-        pos_array = np.zeros((2, nsrcs))
         ha_array = np.zeros((nsrcs, nfits))
-        err_array = np.zeros((1, nsrcs, nfits))
-        data_array = np.zeros((1, nsrcs, nfits))
+        err_array = np.zeros((nsrcs, nfits))
+        data_array = np.zeros((nsrcs, nfits))
         azalt_array = np.zeros((2, nsrcs, nfits))
         jds = self._get_jds(fitsfiles)
         for ii, ra in enumerate(ras):
-            pos_array[0, ii] = ra; pos_array[1, ii] = decs[ii]
             key = (round(ra, 2), round(decs[ii], 2))
             if not key in srcdict: srcdict[key] = {}
             for jj, fn in enumerate(fitsfiles):
                 srcstats = et.get_peakflux(fn, ra, decs[ii])
-                lst = ct.jd2lst(jds[jj])
-                ha = ct.ralst2ha(ra * np.pi/180, lst)                  
-                az, alt = ct.hadec2azalt(decs[ii] * np.pi/180., ha)
+                lst, ha = self._get_lstha(jds[jj], ra)
+                az, alt = self._get_azalt(decs[ii], ha)
                 ha_array[ii, jj] = ha
-                err_array[0, ii, jj] = srcstats['error']
-                data_array[0, ii, jj] = srcstats['flux']
+                err_array[ii, jj] = srcstats['error']
+                data_array[ii, jj] = srcstats['flux']
                 azalt_array[0, ii, jj] = az
                 azalt_array[1, ii, jj] = alt
-            # saving to dictionary
-            srcdict[key]['data'] = data_array[:, ii,  :]
-            srcdict[key]['error'] = err_array[:, ii, :]
+                # saving to dictionary
+            srcdict[key]['data'] = data_array[ii,  :]
+            srcdict[key]['error'] = err_array[ii, :]
             srcdict[key]['ha'] = ha_array[ii, :]
+            srcdict[key]['azalt'] = azalt_array[:, ii, :]
+        return srcdict
+
+    def _srcdict_catdata(self, srcdict):
+        """
+        Created catData object using dictionary containing the data values and required metadata.
+        Parameters
+        ----------
+        srcdict : dict
+            Dictionary containing the data and metadata about the astronomical sources.
+        """ 
         # saving attributes to object
-        self.data_array = data_array
-        self.err_array = err_array
-        self.ha_array = ha_array
-        self.pos_array = pos_array
-        self.azalt_array = azalt_array
-        _sh = data_array.shape
-        self.Npols = _sh[0]
-        self.Nsrcs = _sh[1]
-        self.Nfits = _sh[2]
+        keys = srcdict.keys()
+        self.pos_array = keys
+        _sh1 = len(keys)
+        _sh = srcdict[keys[0]]['data'].shape
+        if len(_sh) == 1:
+            _sh0 = 1; _sh2 = _sh[0]
+        else:
+            _sh0 = _sh[0]; _sh2 = _sh[1]  
+        self.data_array = np.zeros((_sh0, _sh1, _sh2)) 
+        self.err_array = np.zeros((_sh0, _sh1, _sh2))
+        self.ha_array = np.zeros((_sh1, _sh2))
+        self.azalt_array= np.zeros((2, _sh1, _sh2))
+        for ii, key in enumerate(keys):
+            self.data_array[:, ii, :] = srcdict[key]['data']
+            self.err_array[:, ii, :] = srcdict[key]['error']
+            self.ha_array[ii, :] = srcdict[key]['ha']
+            self.azalt_array[:, ii, :] = srcdict[key]['azalt']
+        self.Nsrcs = _sh1
+        self.Nfits = _sh2
+
+    def _combine_srcdict(self, srcdict_xx, srcdict_yy):
+        """
+        Combines data from two dictionaries into a single one
+        srcdict_xx: dict
+            Dictionary containing data and metadata for xx polarization.
+        srcdict_yy: dict
+            Dictionary containing data and metadata for yy polarization.
+        """
+        keys_xx = srcdict_xx.keys()
+        keys_yy = srcdict_yy.keys()
+        srcdict = copy.deepcopy(srcdict_xx)
+        assert keys_xx == keys_yy, "both dictionary should have the same keywords."
+        for key in keys_xx:
+            for skey in ['data', 'error']:
+                srcdict[key][skey] = np.array([srcdict_xx[key][skey], srcdict_yy[key][skey]])
+        return srcdict
+
+    def gen_catalog(self, ras, decs, fitsfiles_xx, fitsfiles_yy=None, pols='xx', return_data=False):
+        """
+        Extracts flux measurements at specified right ascension and declination values from the fitsfiles
+        and generates a catdata object containing the data and necessary metadata for xx or yy or both
+        polarization. It can also return a dictionary containing the data and selected metadata.
+        Parameters
+        ---------
+        ras : list of float
+            List of right ascension values in degrees.
+        decs : list of floats
+            List of declination values in degrees.
+        fitsfiles_xx : list of str
+            List of of xx fitsfiles that will be used to generate or extract the source catalog.
+        fitsfiles_yy : list of str
+            List of of yy fitsfiles that will be used to generate or extract the source catalog.
+        pol : str ot list of str
+            Polizations can be xx or yy or both.
+        return_data : boolean
+            If True, returns dictionary with the data values and selected metadata.
+        """
+        assert len(ras) == len(decs), "Right ascenscion array should be of the same size as declination array."
+        if not isinstance(pols, list): pols = [pols]
+        npols = len(pols)
+        if npols == 1:
+            fitsfiles = fitsfiles_xx if pols[0] == 'xx' else fitsfiles_yy 
+            srcdict = self._generate_srcdict(ras, decs, fitsfiles_xx) 
+        else:
+            srcdict_xx = self._generate_srcdict(ras, decs, fitsfiles_xx)
+            srcdict_yy = self._generate_srcdict(ras, decs, fitsfiles_yy)
+            srcdict = self._combine_srcdict(srcdict_xx, srcdict_yy)        
+        self._srcdict_catdata(srcdict)
+        self.pols = pols
         if return_data:
             return srcdict
 
     def write_hdf5(self, filename, clobber=False):
         """
         Writes catData object to HDF5 file (saves it on disk)
-
         Parameters
         ----------
         filename : str
             Name of output HDF5 file
-
         clobber : boolean
             Option to overwrite the file if it already exists. Default is False.
         """    
-        
-         # checking if file exists
+        # checking if file exists
         if os.path.exists(filename):
             if clobber:
                 print ("Overwriting existing file.")
@@ -169,7 +250,7 @@ class catData(object):
             mgp = f.create_group('Metadata')
             mgp['Nfits'] = self.Nfits
             mgp['Nsrcs'] = self.Nsrcs
-            mgp['Npols'] = self.Npols
+            mgp['pols'] = self.pols
             mgp['ha_array'] = self.ha_array
             mgp['err_array'] = self.err_array
             mgp['pos_array'] = self.pos_array
@@ -180,20 +261,18 @@ class catData(object):
 
     def read_hdf5(self, filename):
         """
-        Read HDF5 and loads the attributes into a catData object
-        
+        Read HDF5 and loads the attributes into a catData object        
         Parameters
         ----------
         filename : str
             Name of input HDF5 file.
         """
-
         with h5py.File(filename, 'r') as f:
             # read meta data information
             mgp = f['Metadata']
             self.Nfits = mgp['Nfits'].value
             self.Nsrcs = mgp['Nsrcs'].value
-            self.Npols = mgp['Npols'].value
+            self.Npols = mgp['pols'].value
             self.ha_array = mgp['ha_array'].value
             self.err_array = mgp['err_array'].value
             self.pos_array = mgp['pos_array'].value
--- beam_solver/tests/test_catdata.py
+++ beam_solver/tests/test_catdata.py
@@ -39,45 +39,92 @@ class Test_catData():
         jds = catd._get_jds([outfile])
         nt.assert_equal(jds, [2458115.23736])
 
-    def test_gen_catalog(self):
+    def test_get_lstha(self):
         catd = cd.catData()
-        catd.gen_catalog([outfile], ras, decs)
-        nt.assert_equal(catd.Npols, 1)
+        jd = 2458115.23736 + 5 / 24. / 60
+        lst, ha = catd._get_lstha(2458115.23736, 22.47460079)
+        np.testing.assert_allclose(ha, 0, atol=1e-1)
+
+    def test_get_azalt(self):
+        catd = cd.catData()
+        az, alt = catd._get_azalt(-30.721388888888885, 0)
+        nt.assert_almost_equal(az, np.pi/2)
+        nt.assert_almost_equal(alt, np.pi/2)
+
+    def test_generate_srcdict(self):
+        catd = cd.catData()
+        srcdict = catd._generate_srcdict(ras, decs, [outfile])
+        keys = srcdict.keys()
+        nt.assert_equal(len(keys), len(ras))
+        data = srcdict[keys[0]]['data']
+        nt.assert_equal(data.shape, (1,))
+        np.testing.assert_almost_equal(data, np.array([0.75]))
+
+    def test_srcdict_catdata(self):
+        catd = cd.catData()
+        srcdict = catd._generate_srcdict(ras, decs, [outfile])
+        catd._srcdict_catdata(srcdict)
+        nt.assert_equal(catd.data_array.shape, (1, len(ras), 1))
+        np.testing.assert_almost_equal(catd.pos_array, srcdict.keys())
+
+    def test_combine_srcdict(self):
+        catd1 = cd.catData()
+        srcdict1 = catd1._generate_srcdict(ras, decs, [outfile])
+        catd2 = cd.catData()
+        srcdict2 = catd2._generate_srcdict(ras, decs, [outfile])
+        catd = cd.catData()
+        srcdict = catd._combine_srcdict(srcdict1, srcdict2)
+        keys = srcdict.keys()
+        data = srcdict[keys[0]]['data']
+        nt.assert_equal(data.shape, (2, 1))
+        np.testing.assert_almost_equal(data, np.array([[0.75], [0.75]]))
+
+    def test_gen_catalog_1pol(self):
+        catd = cd.catData()
+        catd.gen_catalog(ras, decs, fitsfiles_xx=[outfile])
+        nt.assert_equal(catd.pols, ['xx'])
         nt.assert_equal(catd.Nfits, 1)
         nt.assert_equal(catd.Nsrcs, len(ras))
         nt.assert_equal(catd.azalt_array.shape, (2, len(ras), 1))
         nt.assert_equal(catd.data_array.shape, (1, len(ras), 1))
         nt.assert_equal(catd.err_array.shape, (1, len(ras), 1))
-        nt.assert_equal(catd.pos_array.shape, (2, len(ras)))
+        nt.assert_equal(catd.ha_array.shape, (len(ras), 1))
+
+    def test_gen_catalog_2pol(self):
+        catd = cd.catData()
+        catd.gen_catalog(ras, decs, fitsfiles_xx=[outfile], fitsfiles_yy=[outfile], pols=['xx', 'yy'])
+        nt.assert_equal(catd.pols, ['xx', 'yy'])
+        nt.assert_equal(catd.Nfits, 1)
+        nt.assert_equal(catd.Nsrcs, len(ras))
+        nt.assert_equal(catd.azalt_array.shape, (2, len(ras), 1))
+        nt.assert_equal(catd.data_array.shape, (2, len(ras), 1))
+        nt.assert_equal(catd.err_array.shape, (2, len(ras), 1))
         nt.assert_equal(catd.ha_array.shape, (len(ras), 1))
 
     def test_catalog_vals(self):
         catd = cd.catData()
-        catd.gen_catalog([outfile], ras, decs)
-        np.testing.assert_almost_equal(catd.pos_array, np.array([ras, decs]))
+        catd.gen_catalog(ras, decs, [outfile])
         np.testing.assert_almost_equal(catd.data_array, np.array([[[0.75], [0.5], [1.0], [0.5], [0.6]]]))
-
+    
     def test_return_dict(self):
         catd = cd.catData()
-        srcdict = catd.gen_catalog([outfile], ras, decs, return_data=True)
+        srcdict = catd.gen_catalog(ras, decs, [outfile], return_data=True)
         nt.assert_equal(len(srcdict.keys()), len(ras))
-        np.testing.assert_almost_equal(catd.pos_array, np.array([ras, decs]))
-        np.testing.assert_almost_equal(catd.data_array, np.array([[[0.75], [0.5], [1.], [0.5], [0.6]]]))
+        np.testing.assert_almost_equal(srcdict.keys(), catd.pos_array)
 
     def test_write_hdf5(self):
         catd = cd.catData()
-        catd.gen_catalog([outfile], ras, decs)
+        catd.gen_catalog(ras, decs, [outfile])
         catd.write_hdf5(h5file, clobber=True)
         nt.assert_true(os.path.exists(h5file))
         nt.assert_raises(IOError, catd.write_hdf5, h5file)
 
     def test_read_hdf5(self):
         catd = cd.catData()
-        catd.gen_catalog([outfile], ras, decs)
+        catd.gen_catalog(ras, decs, [outfile])
         catd.write_hdf5(h5file, clobber=True)
         catd.read_hdf5(h5file)
         nt.assert_equal(catd.Nsrcs, len(ras))
-        np.testing.assert_almost_equal(catd.pos_array, np.array([ras, decs]))
         np.testing.assert_almost_equal(catd.data_array, np.array([[[0.75], [0.5], [1.], [0.5], [0.6]]]))
         
     def test_no_hdf5(self):
@@ -86,7 +133,7 @@ class Test_catData():
 
     def test_calc_catalog_flux(self):
         catd = cd.catData()
-        catd.gen_catalog([outfile], ras, decs) 
+        catd.gen_catalog(ras, decs, [outfile]) 
         beam = bt.get_fitsbeam(beamfits, 151e6)
         catalog_flux = catd.calc_catalog_flux(beam, 'xx')
         nt.assert_almost_equal(catalog_flux[2], 1.000, 3)
