2b7a27378e297a43360e090d4c7989c0dae1f46a
Chuneeta <cnunhokee@gmail.com>
2019-03-22 15:58:48 -0700

    added gaussian modelling of sources

--- beam_solver/extract.py
+++ beam_solver/extract.py
@@ -2,16 +2,16 @@ import numpy as np
 from beam_solver import fits_utils as ft
 from beam_solver.data import DATA_PATH
 import warnings
+from astropy.modeling import models, fitting
+import pylab
+import copy
 
-def get_peakflux(fitsfile, ra, dec, radius=None, negative=False):
+def get_flux(fitsfile, ra, dec):
     """
     Returns the statistics obtained from the desired or selected region
     fitsfile: Input fitsfile
     ra : Right ascension in degrees
     dec : Declination in degrees
-    radius: Radius or tolerance to select the desired region
-    negative: If True, returns the maximum value irrespective of the
-    ngative sign.
     """
     fitsinfo = ft.get_fitsinfo(fitsfile)
     imdata = fitsinfo['data']
@@ -21,14 +21,15 @@ def get_peakflux(fitsfile, ra, dec, radius=None, negative=False):
     nyaxis = fitsinfo['nyaxis']
     bmaj = hdr['BMAJ']
     bmin = hdr['BMIN']
+    bpa = hdr['BPA']
     # computing synthesized beam radius and area in degrees and pixels
     w = ft._get_wcs(fitsfile)
     dx_px = np.abs(w.wcs.cdelt[0])
     dy_px = np.abs(w.wcs.cdelt[1])
     bmaj_px = bmaj / dx_px
     bmin_px = bmin / dy_px
-    bm_radius =  np.sqrt(bmaj**2 + bmin**2)
-    bm_radius_px = np.sqrt(bmaj_px**2 + bmin_px**2)
+    bm_radius =  np.sqrt(bmaj**2 + bmin**2)/2
+    bm_radius_px = np.sqrt(bmaj_px**2 + bmin_px**2)/2.
     bm_area = bmaj * bmin * np.pi / 4 / np.log(2)
     px_area = dx_px * dy_px
     bm_npx = bm_area / px_area
@@ -46,15 +47,24 @@ def get_peakflux(fitsfile, ra, dec, radius=None, negative=False):
         imdata_select = imdata[select]
         peakval = np.nanmax(imdata_select)
         minval = np.nanmin(imdata_select)
-        if negative:
-            if np.abs(peakval) < np.abs(minval):
-                peakval = minval
-        std = np.nanstd(imdata_select)
-        rms = np.sqrt(np.nanmean(imdata[~select])**2) # calculated rms outside the selected region
-        peak_err = std / bm_npx # std/sqrt(N)
-        flux, err = peakval, peak_err
+        # fitting gaussian to point sources
+        gauss_data = copy.deepcopy(imdata)
+        gauss_data[~select] = 0            
+        gauss_data = gauss_data.reshape((nxaxis, nyaxis))
+        inds = np.unravel_index(np.argmax(gauss_data), (nxaxis, nyaxis))
+        mod = models.Gaussian2D(peakval, inds[1], inds[0], bmaj_px/2, bmin_px/2, theta=bpa * np.pi/180)
+        fit_p = fitting.LevMarLSQFitter()
+        with warnings.catch_warnings():
+            # Ignore model linearity warning from the fitter
+            warnings.simplefilter('ignore')
+            gauss_mod = fit_p(mod, ll, mm, gauss_data)
+        gauss_peak = gauss_mod.amplitude.value
+        fitted_data = gauss_mod(ll, mm)
+        residual = gauss_data - fitted_data
+        gauss_int = np.sum(fitted_data) / bm_npx
+        gauss_err = np.std(residual[select])
+        peak_flux, int_flux, gauss_err = gauss_peak, gauss_int, gauss_err
     else:
         warnings.warn('WARNING: Right ascension or declination outside image field, therefore values are set to nan', Warning)
-        flux, err, std = np.nan, np.nan, np.nan
-    
-    return {'freq': freq, 'flux': flux, 'error': err, 'std': std}
+        peakval, peak_flux, int_flux, gauss_err = np.nan, np.nan, np.nan, np.nan
+    return {'freq': freq, 'pflux': peakval, 'gauss_pflux': peak_flux, 'gauss_tflux':int_flux, 'error': gauss_err}
