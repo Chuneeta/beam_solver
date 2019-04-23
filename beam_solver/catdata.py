0eca44e5fc97e23b4226290b7f362857e04fb6f0
Chuneeta <cnunhokee@gmail.com>
2019-04-22 23:16:38 -0700

    fixed nan in get_azalt

--- beam_solver/catdata.py
+++ beam_solver/catdata.py
@@ -121,6 +121,7 @@ class catData(object):
         ha : float 
             Hour angle in degrees.       
         """
+        ha[ha == 0.] = 1e-7
         az, alt = ct.hadec2azalt(dec * np.pi/180., ha)
         return az, alt
 
