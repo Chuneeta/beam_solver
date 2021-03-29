49ac0238203e9cc211eaab436908a852e04f6703
Ridhima Nunhokee <ridhima@Ridhimas-MacBook-Pro.local>
2021-03-29 18:01:16 +0500

    added functionality to plot fitsfile

--- beam_solver/fits_utils.py
+++ beam_solver/fits_utils.py
@@ -107,3 +107,15 @@ def calc_solint(fitsfile, nants, inttime):
     solint = (std / mxval)**2 * (inttime * (nants - 3)) / 9
     return solint
 
+def plot_fitsfile(fitsfile, cmap='gray', vmin=None, vmax=None):
+    _info = get_fitsinfo(fitsfile)
+    hdr = _info['hdr']
+    my_wcs = wcs.WCS(hdr, naxis=[wcs.WCSSUB_CELESTIAL])
+    fig = pylab.figure()
+    ax = fig.add_subplot(111, projection=my_wcs)
+    im = ax.imshow(_info['data'].squeeze(), aspect='auto', vmin=vmin, vmax=vmax, cmap=cmap)
+    cbar = pylab.colorbar(im, ax=ax)
+    ax.grid(ls='dotted', color='white')
+    ax.coords[0].set_axislabel('RA (deg)')
+    ax.coords[1].set_axislabel('DEC (deg)')
+    pylab.show()
